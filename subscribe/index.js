491f3c1d1c5ec7c191dd5f06694cddbe9892788d
Andreza Pessoa <andreza.21pessoa@gmail.com>
2023-05-06 17:20:02 -0300

    ajustes na promisse

--- subscribe/index.js
+++ subscribe/index.js
@@ -3,11 +3,13 @@ const { share} = rxjs.operators;
 
 // PROMISSE VS OBSERVEBLE
 const promise = new Promise((resolve) => {
-    console.log('Iniciando nova Promisse');
+    console.log('new Promisse');
+    console.log('new Promisse');
+    console.log('new Promisse');
     setTimeout(() => resolve(1));
 });
 
 const newPromisse = new Promise((resolve) => {
-    console.log('Iniciando  Promisse 2');
-    setTimeout(() => resolve(1), 3000);
+    console.log('promisse 2');
+    setTimeout(() => resolve(1), 2000);
 });
