f5726fbf79294151dc82815d613a0e0eae69ca6a
Andreza Pessoa <andreza.21pessoa@gmail.com>
2023-05-06 18:37:38 -0300

    adicionando uma nova promise

--- subscribe/index.js
+++ subscribe/index.js
@@ -11,5 +11,11 @@ const promise = new Promise((resolve) => {
 
 const newPromisse = new Promise((resolve) => {
     console.log('promisse 2');
+    const teste = 5 + 3;
+    console.log('teste1 ',teste);
     setTimeout(() => resolve(1), 2000);
+});
+
+const mercado = new Promise((resolve) => {
+    console.log('mercado', resolve(1));
 });
