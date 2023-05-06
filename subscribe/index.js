a0ec8da7fa6abda5147f0349d045c287e28e1c84
Andreza Pessoa <andreza.21pessoa@gmail.com>
2023-05-06 17:10:09 -0300

    criando uma nova promisse

--- subscribe/index.js
+++ subscribe/index.js
@@ -3,6 +3,11 @@ const { share} = rxjs.operators;
 
 // PROMISSE VS OBSERVEBLE
 const promise = new Promise((resolve) => {
-    console.log('Iniciando Promisse 1');
+    console.log('Iniciando nova Promisse');
+    setTimeout(() => resolve(1), 2000);
+});
+
+const newPromise = new Promise((resolve) => {
+    console.log('Iniciando  Promisse 2');
     setTimeout(() => resolve(1), 3000);
 });
